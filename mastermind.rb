6a455d6120d7c0080753f1ffdc9c45cc5a92143e
Samiul Anwar <samiul.anwar@gmail.com>
2016-03-30 18:03:46 -0400

    trying to figure out how to update a single row based on user input

--- mastermind.rb
+++ mastermind.rb
@@ -1,4 +1,7 @@
 class Board
+
+
+
 	def colors(color1=" ",color2=" ",color3=" ",color4=" ")
 		"[#{color1}] [#{color2}] [#{color3}] [#{color4}]"
 	end
@@ -7,10 +10,20 @@ class Board
 		"#{key1}"+"#{key2}"+"#{key3}"+"#{key4}"
 	end
 
-	def row
-		puts colors+"   "+keys("+","+","-")
+	def row(c1=" ",c2=" ",c3=" ",c4=" ",k1="o",k2="o",k3="o",k4="o")
+		puts colors(c1,c2,c3,c4)+"   "+keys(k1,k2,k3,k4)
 	end
+
+	def empty_row
+		puts colors+"   "+keys
+	end
+
+	def board
+		11.times{empty_row}
+		1.times{row()}
+	end
+
 end
 
 b=Board.new
-b.row
+b.board
